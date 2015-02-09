20e0ff647d8bb07d26a91ffcae8bc126deab1893
Ji Lee <gafrong@gmail.com>
2015-02-10 09:33:30 +1100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,22 @@ parsed.each do |product|
   new_product.name = product["name"]
   new_product.image = product["image"]
   new_product.price = product["price"]
-  new_product.retailer = product["retailer"].to_s
-  if new_product.retailer == "YourGrocer"
+  new_product.retailer = product["retailer"]
+  if new_product.retailer.name == "YourGrocer"
     new_product.retailer_id = 1
-  elsif new_product.retailer == "Rustica Sourdough Bakery"
+  elsif new_product.retailer.name == "Rustica Sourdough Bakery"
     new_product.retailer_id = 2
-  elsif new_product.retailer == "North Carlton Quality Meats"
+  elsif new_product.retailer.name == "North Carlton Quality Meats"
     new_product.retailer_id = 3
-  elsif new_product.retailer == "Melba's Food Hall"
+  elsif new_product.retailer.name == "Melba's Food Hall"
     new_product.retailer_id = 4
-  elsif new_product.retailer == "La Manna Fresh"
+  elsif new_product.retailer.name == "La Manna Fresh"
     new_product.retailer_id = 5
-  elsif new_product.retailer == "Organics by La Manna Fresh"
+  elsif new_product.retailer.name == "Organics by La Manna Fresh"
     new_product.retailer_id = 6
-  elsif new_product.retailer == "Taranto's Balwyn Fruit Supply"
+  elsif new_product.retailer.name == "Taranto's Balwyn Fruit Supply"
     new_product.retailer_id = 7
-  elsif new_product.retailer == "Kandi's Balwyn Heights Fruit Supply"
+  elsif new_product.retailer.name == "Kandi's Balwyn Heights Fruit Supply"
     new_product.retailer_id = 8
   end      
       
