2569eb4e4d4daddf5c6215c3d2b3bde07a6c8dcb
Ji Lee <gafrong@gmail.com>
2015-02-10 10:34:33 +1100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'pry'
+
 
 parsed = JSON.parse(File.read("db/products.json"))
 
