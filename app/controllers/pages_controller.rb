dbe12e81ab40e4ae9d42a3a1241290fd7efb1855
Ji Lee <gafrong@gmail.com>
2015-02-10 09:14:19 +1100

    seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def index
-    @products = Product.all.page params[:page]
+    @products = Product.all.page(params[:page])
     @retailers = Retailer.all 
   end
 
