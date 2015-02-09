7aa83b18437284fd8746e2f25b70fd13b3d41d7f
Ji Lee <gafrong@gmail.com>
2015-02-10 09:27:22 +1100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,21 +12,21 @@ parsed.each do |product|
   new_product.price = product["price"]
   new_product.retailer = product["retailer"]
   if new_product.retailer == "YourGrocer"
-    new_product.retailer_id = 1.to_i
+    new_product.retailer_id = 1
   elsif new_product.retailer == "Rustica Sourdough Bakery"
-    new_product.retailer_id = 2.to_i
+    new_product.retailer_id = 2
   elsif new_product.retailer == "North Carlton Quality Meats"
-    new_product.retailer_id = 3.to_i
+    new_product.retailer_id = 3
   elsif new_product.retailer == "Melba's Food Hall"
-    new_product.retailer_id = 4.to_i
+    new_product.retailer_id = 4
   elsif new_product.retailer == "La Manna Fresh"
-    new_product.retailer_id = 5.to_i
+    new_product.retailer_id = 5
   elsif new_product.retailer == "Organics by La Manna Fresh"
-    new_product.retailer_id = 6.to_i
+    new_product.retailer_id = 6
   elsif new_product.retailer == "Taranto's Balwyn Fruit Supply"
-    new_product.retailer_id = 7.to_i
+    new_product.retailer_id = 7
   elsif new_product.retailer == "Kandi's Balwyn Heights Fruit Supply"
-    new_product.retailer_id = 8.to_i
+    new_product.retailer_id = 8
   end      
       
   new_product.save
